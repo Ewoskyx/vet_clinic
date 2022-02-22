cd87ecfff3e55fc77f602d348e18c49f148f3b88
KayLemba <kaylemba@yahoo.com>
2022-02-22 19:18:44 +0200

    Add indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,7 +11,10 @@ CREATE TABLE medical_histories (
     id INTEGER NOT NULL,
     admitted_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     patient_id INTEGER NOT NULL,
-    status VARCHAR(255) NOT NULL
+    status VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_patient
+    FOREIGN KEY(patient_id)
+    REFERENCES patients(id)
 );
 ALTER TABLE
     medical_histories ADD PRIMARY KEY(id);
@@ -20,6 +23,7 @@ CREATE TABLE treatments (
     id INTEGER NOT NULL,
     type VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL
+
 );
 ALTER TABLE
     treatments ADD PRIMARY KEY(id);
@@ -30,6 +34,9 @@ CREATE TABLE invoices (
     generated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     payed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     medical_history__id INTEGER NOT NULL
+    CONSTRAINT fk_medical_history
+    FOREIGN KEY (medical_history_id) 
+    REFERENCES medical_histories(id)
 );
 ALTER TABLE
     invoices ADD PRIMARY KEY(id);
@@ -41,6 +48,12 @@ CREATE TABLE invoice_items (
     total_price DECIMAL(8, 2) NOT NULL,
     invoice_id INTEGER NOT NULL,
     treatment_id INTEGER NOT NULL
+    CONSTRAINT fk_invoice 
+    FOREIGN KEY (invoice_id) 
+    REFERENCES invoices(id),
+    CONSTRAINT fk_treatment 
+    FOREIGN KEY (treatment_id) 
+    REFERENCES treatments(id)
 );
 
 
@@ -54,3 +67,9 @@ ALTER TABLE
     invoices ADD CONSTRAINT invoices_medical_history__id_foreign FOREIGN KEY(medical_history__id) REFERENCES medical_histories(id);
 ALTER TABLE
     invoice_items ADD CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id);
+
+--Indexes 
+CREATE INDEX patient_id_index ON medical_histories(patient_id);
+CREATE INDEX medical_id_index ON invoices(medical_history_id);
+CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treat_id_index ON invoice_items(treatment_id);
